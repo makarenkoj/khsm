8346ae542ca55a93af14151b438a5dd74c6d79ca
makarenkoj <makarenkoj@gmail.com>
2020-01-12 23:27:03 +0200

    test #show alient game

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -91,5 +91,16 @@ RSpec.describe GamesController, type: :controller do
       expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
       expect(response).to redirect_to(game_path(game))
     end
+
+    #62/1
+    it '#show alien game' do
+      alien_game = FactoryBot.create(:game_with_questions)
+
+      get :show, id: alien_game.id
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be
+    end
   end
 end
